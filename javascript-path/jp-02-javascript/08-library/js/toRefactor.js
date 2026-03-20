062d44569a4c16a59789f0957bf90ea6f8a4a623
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-20 18:03:10 -0300

    minor change

--- javascript-path/jp-02-javascript/08-library/js/toRefactor.js
+++ javascript-path/jp-02-javascript/08-library/js/toRefactor.js
@@ -1,3 +1,25 @@
+/* DARK MODE */
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkMode = () => {
+    document.documentElement.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+};
+const disableDarkMode = () => {
+    document.documentElement.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active"){
+    enableDarkMode();
+}
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
+
 /* LIBRARY */
 const myLibrary = [];
 let bookIdToTrash = null;
