0b174286601ac54d837cf7e0f19339df578b31dd
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-24 00:34:48 -0300

    add b ody background

--- javascript-path/jp-02-javascript/10-restaurant-page/src/js/home.js
+++ javascript-path/jp-02-javascript/10-restaurant-page/src/js/home.js
@@ -48,7 +48,7 @@ class Home{
         </div>
     `;
     constructor(){}
-    appContainer () {
+    displayHome () {
         const divContent = document.querySelector("#content");
 
         divContent.insertAdjacentHTML('beforeend', this.homeComponent);
@@ -56,3 +56,32 @@ class Home{
 }
 
 export const renderHome = new Home();
+
+class GenerateBackgroundFlames{
+    constructor(){
+        this.bgContainer = document.getElementById('background-flames');
+        this.flameAmount = 25; 
+        this.flameSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"/></svg>`;
+    }
+
+    flameGenerator(){
+        for (let i = 0; i < this.flameAmount; i++) {
+            const flameWrapper = document.createElement('div');
+            flameWrapper.classList.add('flame-icon');
+            flameWrapper.innerHTML = this.flameSVG;
+
+            const randomTop = Math.floor(Math.random() * 100);
+            const randomLeft = Math.floor(Math.random() * 100);
+        
+            flameWrapper.style.top = `${randomTop}vh`;
+            flameWrapper.style.left = `${randomLeft}vw`;
+
+            const randomScale = Math.random() * 0.6 + 0.5;
+            flameWrapper.style.transform = `scale(${randomScale})`;
+
+            this.bgContainer.appendChild(flameWrapper);
+        }
+    }
+}
+
+export const flameGenerator = new GenerateBackgroundFlames();
