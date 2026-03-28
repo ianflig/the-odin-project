6f6c827c5250621e9f3af4ca0d075602f09c8fa5
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-28 10:58:08 -0300

    modify basic structure

--- javascript-path/jp-02-javascript/11-to-do-list/script.js
+++ javascript-path/jp-02-javascript/11-to-do-list/script.js
@@ -1,12 +1,3 @@
-class Category {
-    constructor(title, description) {
-        this.id = crypto.randomUUID();
-        this.title = title;
-        this.description = description;
-        this.tasks = [];
-    }
-}
-
 class Task {
     constructor(title, dueDate, description, priority, status) {
         this.id = crypto.randomUUID();
@@ -18,39 +9,83 @@ class Task {
     }
 }
 
+class Category {
+    tasks = []
+    constructor(title, description) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.tasks = this.tasks;
+    }
+
+    addTask(task){
+        this.tasks.push(task);
+    }
+
+    deleteTask(){
+
+    }
+
+    getTaskByID(id){
+        let task = this.tasks.find(ele => ele.id === id)
+        return task;
+    }
+}
+
+
+
 class Storage{
     vault = [];
     constructor(){}
 
-    setCategory(category){
+    addCategory(category){
         this.vault.push(category);
         console.log(`Category created, ID: ${category.id}`) 
     }
 
-    setTask(task, category){
-        for (let i = 0; i < this.vault.length; i++){
-            if (this.vault[i].id === category){
-                this.vault[i].tasks.push(task);
-            }
-        }
+    deleteCategory(){
+
+    }
+
+    getCategoryByID(id){
+        let category = this.vault.find(ele => ele.id === id);
+        return category;
     }
 }
 
 class Controller{
-    constructor(vault){
-        this.vault = vault;
+    constructor(storage){
+        this.storage = storage;
     }
 
-    addCategory(title, description){
+    createCategory(title = "New Category", description = "..."){
         let category = new Category(title, description);
-        this.vault.setCategory(category);
+        this.storage.addCategory(category);
     }
 
-    addTask(title, dueDate, description, priority, status, category){
+    createTask(categoryID, title = "New Task", dueDate, description = "...", priority = "Medium", status = false){
         let task = new Task(title, dueDate, description, priority, status);
-        this.vault.setTask(task, category);
+        let category = this.storage.getCategoryByID(categoryID)
+        category.addTask(task);
+    }
+
+    editCategory(){
+
+    }
+
+    editTask(){
+    }
+
+    deleteCategory(){
+
+    }
+
+    deleteTask(){
+
     }
 }
+
+
 
 /* index.js */
 
