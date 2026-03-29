78bf0116891369e97c0746f8f5e162726b60a7bd
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-28 22:10:07 -0300

    import darkMode & UI structure progress

--- javascript-path/jp-02-javascript/11-to-do-list/src/index.js
+++ javascript-path/jp-02-javascript/11-to-do-list/src/index.js
@@ -1,8 +1,10 @@
 import "./css/styles.css";
 import { Storage } from "./js/Storage.js";
 import { Controller } from "./js/Controller.js";
+import { darkMode } from "./js/darkMode.js";
 
 const storage = new Storage();
 const app = new Controller(storage);
 
+new darkMode();
 window.app = app;
