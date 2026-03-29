b353c8aeb6ce0a0c9dc113dcef5ad17933c0dd30
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-29 18:04:09 -0300

    finish category create & edit

--- javascript-path/jp-02-javascript/11-to-do-list/src/js/ScreenController.js
+++ javascript-path/jp-02-javascript/11-to-do-list/src/js/ScreenController.js
@@ -12,11 +12,20 @@ class DialogManager{
     }
 
     bindEvents(){
-        this.categoryDialog.addEventListener("close", () => {this.categoryForm.reset();})
+        this.categoryDialog.addEventListener("close", () => {
+            this.categoryForm.reset();
+            document.querySelector("#hidden-category-id").value = "";
+        })
         this.taskDialog.addEventListener("close", () => {this.taskForm.reset()});
     }
 
     openCategoryModal(){
+        this.categoryDialog.showModal();
+    }
+
+    openCategoryModalForEdit(categoryToEdit){
+        document.querySelector("#category-title").value = categoryToEdit.title;
+        document.querySelector("#hidden-category-id").value = categoryToEdit.id;
         this.categoryDialog.showModal();
     }
 
@@ -51,18 +60,23 @@ class DOMRenderer{
 
         categoryArray.forEach(ele => {
             const button = document.createElement("button");
+            const buttonSpan = document.createElement("span");
             const divIcons = document.createElement("div");
             const editSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             const deleteSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-            button.textContent = ele.title;
+            buttonSpan.textContent = ele.title;
             button.classList.add("category-btn");
+            button.id = "category-btn"
             button.dataset.id = ele.id;
+            editSVG.id = "edit-category-btn";
+            deleteSVG.id = "delete-category-btn";
             editSVG.innerHTML = '<use href="#edit-icon"></use>';
             deleteSVG.innerHTML = '<use href="#delete-icon"></use>';
             divIcons.classList.add("icons");
             divIcons.appendChild(editSVG);
             divIcons.appendChild(deleteSVG);
+            button.appendChild(buttonSpan);
             button.appendChild(divIcons);
             this.categoryContainer.appendChild(button);
         });
@@ -79,6 +93,7 @@ export class ScreenController{
         this.closeCategoryBtn = document.querySelector("#category-close-btn");
         this.saveCategoryBtn = document.querySelector("#category-save-btn");
         this.categoryForm = document.querySelector("#category-form");
+        this.categoryContainer = document.querySelector("#category-container");
 
         this.newTaskBtn = document.querySelector("#new-task-btn");
         this.closeTaskbtn = document.querySelector("#task-close-btn");
@@ -91,21 +106,37 @@ export class ScreenController{
     bindEvents(){
         this.newCategoryBtn.addEventListener("click", () => {this.dialogs.openCategoryModal()});
         this.closeCategoryBtn.addEventListener("click", () => {this.dialogs.closeCategoryModal()});
-        this.categoryForm.addEventListener("submit", (e) => {this.saveCategoryHandler(e)});
+        this.categoryContainer.addEventListener("click", (e) => {this.categoryContainerHandler(e)});
+        this.categoryForm.addEventListener("submit", (e) => {this.categorySubmitHandler(e)});
 
         this.newTaskBtn.addEventListener("click", () => {this.dialogs.openTaskModal()});
         this.closeTaskbtn.addEventListener("click", () => {this.dialogs.closeTaskModal()});
         this.saveTaskBtn.addEventListener("submit", (e) => {this.saveTaskHandler(e)});
     }
 
-    saveCategoryHandler(e){
+    categorySubmitHandler(e){
         e.preventDefault();
-
         const formData = Object.fromEntries(new FormData(this.categoryForm));
-        console.log(formData);
-        this.controller.createCategory(formData);
-        this.renderer.renderCategories(this.controller.storage.vault);
+
+        if (formData.categoryId){
+            this.controller.editCategory(formData.categoryId, formData);
+        } else {
+            this.controller.createCategory(formData);
+        }
+
         this.dialogs.closeCategoryModal();
+        this.renderer.renderCategories(this.controller.storage.vault);
+    }
+
+    categoryContainerHandler(e){
+        e.preventDefault();
+        const editBtn = e.target.closest("#edit-category-btn");
+        const categoryContainer = e.target.closest("#category-btn");
+        if (editBtn){
+            const category = this.controller.storage.getCategoryByID(categoryContainer.dataset.id)
+
+            this.dialogs.openCategoryModalForEdit(category);
+        }
     }
 
     saveTaskHandler(e){
