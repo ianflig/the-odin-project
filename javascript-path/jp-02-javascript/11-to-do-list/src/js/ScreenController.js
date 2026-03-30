c08619046b35efc62127f7790532c66e72b54cd4
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-30 11:51:36 -0300

    refactor delete category & tasks renderer

--- javascript-path/jp-02-javascript/11-to-do-list/src/js/ScreenController.js
+++ javascript-path/jp-02-javascript/11-to-do-list/src/js/ScreenController.js
@@ -7,17 +7,27 @@ class DialogManager{
 
         this.taskDialog = document.querySelector("#task-dialog");
         this.taskForm = document.querySelector("#task-form");
+        this.taskWarningDialog = document.querySelector("#task-warning-dialog")
 
         this.bindEvents();
     }
 
     bindEvents(){
+        /* ---- CATEGORY ---- */
         this.categoryDialog.addEventListener("close", () => {
             this.categoryForm.reset();
             document.querySelector("#hidden-category-id").value = "";
         })
         this.taskDialog.addEventListener("close", () => {this.taskForm.reset()});
-        this.deleteDialog.addEventListener("close", () => {document.querySelector("#delete-confirm-btn").dataset.id = "";})
+        this.deleteDialog.addEventListener("close", () => {document.querySelector("#delete-confirm-btn").dataset.category = "";})
+
+        /* ---- TASK ---- */
+        this.taskDialog.addEventListener("close", () => {
+            this.taskForm.reset();
+            document.querySelector("#hidden-task-id").value = "";
+        })
+        this.taskDialog.addEventListener("close", () => {this.taskForm.reset()});
+        this.deleteDialog.addEventListener("close", () => {document.querySelector("#delete-confirm-btn").dataset.task = "";})
     }
 
     openCategoryModal(){
@@ -42,8 +52,16 @@ class DialogManager{
         this.taskDialog.close();
     }
 
+    openTaskWarningModal(){
+        this.taskWarningDialog.showModal();
+    }
+
+    closeTaskWarningModal(){
+        this.taskWarningDialog.close();
+    }
+
     openDeleteModal(categoryToDelete){
-        document.querySelector("#delete-confirm-btn").dataset.id = categoryToDelete.id;
+        document.querySelector("#delete-confirm-btn").dataset.category = categoryToDelete.id;
         this.deleteDialog.showModal();
     }
 
@@ -55,11 +73,12 @@ class DialogManager{
 class DOMRenderer{
     constructor(){
         this.categoryContainer = document.querySelector("#category-container");
+        this.allTasks = document.querySelector(".all-tasks");
     }
 
     renderCategories(categoryArray){
         this.categoryContainer.innerHTML = "";
-
+        
         categoryArray.forEach(ele => {
             const button = document.createElement("button");
             const buttonSpan = document.createElement("span");
@@ -69,7 +88,7 @@ class DOMRenderer{
 
             buttonSpan.textContent = ele.title;
             button.classList.add("category-btn");
-            button.dataset.id = ele.id;
+            button.dataset.category = ele.id;
             editSVG.id = "edit-category-btn";
             deleteSVG.id = "delete-category-btn";
             editSVG.innerHTML = '<use href="#edit-icon"></use>';
@@ -82,9 +101,60 @@ class DOMRenderer{
             this.categoryContainer.appendChild(button);
         });
     }
+
+    renderTasks(category){
+        if (!category) {
+            document.querySelector("#new-task-btn").dataset.category = "";
+            document.querySelector("#current-category").textContent = "Select or create a new category";
+            this.allTasks.innerHTML = ""
+            return};
+
+        document.querySelector("#new-task-btn").dataset.category = category.id;
+        document.querySelector("#current-category").textContent = category.title;
+        this.allTasks.innerHTML = "";
+        if (!category.tasks) return;
+        category.tasks.forEach(ele => {
+            const divTask = document.createElement("div");
+            const divTaskTitle = document.createElement("div");
+            const h4 = document.createElement("h4");
+            const divPriority = document.createElement("div");
+            const description = document.createElement("p");
+            const divIconContainer = document.createElement("div");
+            const checkSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            const editSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            const deleteSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+            divTask.dataset.task = ele.id;
+            divTask.classList.add("task");
+            divTaskTitle.classList.add("task-title-container")
+            h4.textContent = ele.title;
+            divPriority.classList.add(ele.priority);
+            divPriority.textContent = ele.priority;
+            description.textContent = ele.description;
+            divIconContainer.classList.add("icon-container");
+            checkSVG.classList.add("check-btn")
+            checkSVG.innerHTML = '<use href="#check-icon"></use>';
+            editSVG.classList.add("edit-btn");
+            editSVG.innerHTML = '<use href="#edit-icon"></use>';
+            deleteSVG.classList.add("delete-btn");
+            deleteSVG.innerHTML = '<use href="#delete-icon"></use>';
+
+            divTaskTitle.appendChild(h4);
+            divTaskTitle.appendChild(divPriority);
+            divTask.appendChild(divTaskTitle);
+            divTask.appendChild(description);
+            divIconContainer.appendChild(checkSVG);
+            divIconContainer.appendChild(editSVG);
+            divIconContainer.appendChild(deleteSVG);
+            divTask.appendChild(divIconContainer);
+            this.allTasks.appendChild(divTask);
+        })
+        
+    }
 }
 
 export class ScreenController{
+    currentCaterogyId = null;
     constructor(appLogic){
         this.dialogs = new DialogManager();
         this.renderer = new DOMRenderer();
@@ -100,9 +170,11 @@ export class ScreenController{
         this.closeDeleteBtn = document.querySelector("#delete-close-btn");
 
         this.newTaskBtn = document.querySelector("#new-task-btn");
-        this.closeTaskbtn = document.querySelector("#task-close-btn");
+        this.closeTaskBtn = document.querySelector("#task-close-btn");
         this.saveTaskBtn = document.querySelector("#task-save-btn");
         this.taskForm = document.querySelector("#task-form");
+        this.taskContainer = document.querySelector("#task-container");
+        this.closeWarningTaskDialog = document.querySelector("#task-warning-close-btn");
 
         this.bindEvents();
     }
@@ -116,45 +188,103 @@ export class ScreenController{
         this.confirmDeleteBtn.addEventListener("click", (e) => {this.deleteHandler(e)});
         this.closeDeleteBtn.addEventListener("click", () => {this.dialogs.closeDeleteModal()});
 
-        this.newTaskBtn.addEventListener("click", () => {this.dialogs.openTaskModal()});
-        this.closeTaskbtn.addEventListener("click", () => {this.dialogs.closeTaskModal()});
-        this.saveTaskBtn.addEventListener("submit", (e) => {this.saveTaskHandler(e)});
+        this.newTaskBtn.addEventListener("click", () => {
+            if (!this.newTaskBtn.dataset.category) return this.dialogs.openTaskWarningModal();
+            this.dialogs.openTaskModal()
+        });
+        this.closeTaskBtn.addEventListener("click", () => {this.dialogs.closeTaskModal()});
+        this.taskContainer.addEventListener("click", (e) => {this.taskContainerHandler(e)});
+        this.taskForm.addEventListener("submit", (e) => {this.taskSubmitHandler(e)});
+        this.closeWarningTaskDialog.addEventListener("click", () => {this.dialogs.closeTaskWarningModal()})
     }
 
     categorySubmitHandler(e){
         e.preventDefault();
         const formData = Object.fromEntries(new FormData(this.categoryForm));
 
+        let categoryIdToRender;
+
         if (formData.categoryId){
             this.controller.editCategory(formData.categoryId, formData);
+            categoryIdToRender = formData.categoryId;
         } else {
-            this.controller.createCategory(formData);
+            const newCategory = this.controller.createCategory(formData);
+            categoryIdToRender = newCategory.id;
         }
 
+        const categoryToRender = this.controller.storage.getCategoryByID(categoryIdToRender);
+
+        this.currentCaterogyId = categoryToRender.id;
         this.dialogs.closeCategoryModal();
         this.renderer.renderCategories(this.controller.storage.vault);
+        this.renderer.renderTasks(categoryToRender);
     }
 
     categoryContainerHandler(e){
         e.preventDefault();
         const editBtn = e.target.closest("#edit-category-btn");
         const deleteBtn = e.target.closest("#delete-category-btn");
-        const categoryContainer = e.target.closest(".category-btn");
-        const category = this.controller.storage.getCategoryByID(categoryContainer.dataset.id);
+        const categoryBtn = e.target.closest(".category-btn")
+        const category = this.controller.storage.getCategoryByID(categoryBtn.dataset.category);
 
         if (editBtn){
             this.dialogs.openCategoryModalForEdit(category);
+            return;
         }
         if (deleteBtn) {
             this.dialogs.openDeleteModal(category);
+            return;
         }
+        if (categoryBtn){
+            this.renderer.renderTasks(category);
+            this.currentCaterogyId = category.id;
+            return; 
+        }
+    }
+
+    taskSubmitHandler(e){
+        e.preventDefault();
+        const formData = Object.fromEntries(new FormData(this.taskForm));
+
+        if (formData.taskId){
+            this.controller.editTask(formData.taskId, formData);
+        } else {
+            this.controller.createTask(categoryId, formData);
+        }
+
+        this.dialogs.closeTaskModal();
+        /* this.renderer.renderTasks(this.controller.storage.vault); */
+    }
+
+    taskContainerHandler(e){
+/*         e.preventDefault();
+        const editBtn = e.target.closest("#edit-category-btn");
+        const deleteBtn = e.target.closest("#delete-category-btn");
+        const categoryBtn = e.target.closest(".category-btn")
+        const category = this.controller.storage.getCategoryByID(categoryBtn.dataset.category);
+
+        if (editBtn){
+            this.dialogs.openCategoryModalForEdit(category);
+            return;
+        }
+        if (deleteBtn) {
+            this.dialogs.openDeleteModal(category);
+            return;
+        }
+        if (categoryBtn){
+            this.renderer.renderTasks(category);
+            return; 
+        } */
     }
 
     deleteHandler(e){
         e.preventDefault();
-        const categoryId = document.querySelector("#delete-confirm-btn").dataset.id
+        const categoryId = document.querySelector("#delete-confirm-btn").dataset.category
         this.controller.deleteCategory(categoryId);
         this.renderer.renderCategories(this.controller.storage.vault);
+        if (this.currentCaterogyId === categoryId) {
+            this.currentCaterogyId = null;
+            this.renderer.renderTasks(null)};
         this.dialogs.closeDeleteModal();
     }
 
