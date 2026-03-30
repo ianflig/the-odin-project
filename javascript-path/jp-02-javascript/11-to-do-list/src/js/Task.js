2180fc6186c9515533723425ebe3d7ae34491453
ianflig <129326345+ianflig@users.noreply.github.com>
2026-03-30 15:30:14 -0300

    remove dueDate input

--- javascript-path/jp-02-javascript/11-to-do-list/src/js/Task.js
+++ javascript-path/jp-02-javascript/11-to-do-list/src/js/Task.js
@@ -1,14 +1,13 @@
 export class Task {
     constructor({
         title, 
-        dueDate,
         description, 
         priority = "Low",
         status = false
     }) {
         this.id = crypto.randomUUID();
         this.title = (title && title.trim() !== "") ? title.trim() : "New Task";
-        this.dueDate = (dueDate === "") ? new Date().toISOString().split('T')[0] : dueDate;
+        this.dueDate = new Date().toISOString().split('T')[0];
         this.description = (description && description.trim() !== "") ? description.trim() : "No Description";
         this.priority = priority;
         this.status = status;
