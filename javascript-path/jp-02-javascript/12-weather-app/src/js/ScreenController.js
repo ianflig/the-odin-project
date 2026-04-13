d0b90a07313c9c38bcb5c99d7a669b13f1b517ee
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-12 23:27:14 -0300

    fix initiateApp

--- javascript-path/jp-02-javascript/12-weather-app/src/js/ScreenController.js
+++ javascript-path/jp-02-javascript/12-weather-app/src/js/ScreenController.js
@@ -13,19 +13,14 @@ export class ScreenController {
 
       if (permissions.state === "granted") {
         const userLocation = await this.controller.getUserCurrentPosition();
-        await this.updateScreenData(
-          `${userLocation.latitude}, ${userLocation.longitude}`,
-        );
+        await this.updateScreenData(userLocation);
       } else if (permissions.state === "denied") {
-        await this.controller.getDataFromAPI();
         await this.updateScreenData();
       } else if (permissions.state === "prompt") {
-        await this.controller.getDataFromAPI();
+        this.updateScreenData();
         try {
           const result = await this.controller.getUserCurrentPosition();
-          await this.updateScreenData(
-            `${result.latitude}, ${result.longitude}`,
-          );
+          await this.updateScreenData(result);
         } catch (rejectionError) {
           console.log(rejectionError);
         }
