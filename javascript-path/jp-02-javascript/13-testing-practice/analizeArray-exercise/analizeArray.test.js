0f7d00e26fe7fc499136c7202e8cc7e4fe82d55d
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-21 11:35:16 -0300

    triangulation

--- javascript-path/jp-02-javascript/13-testing-practice/analizeArray-exercise/analizeArray.test.js
+++ javascript-path/jp-02-javascript/13-testing-practice/analizeArray-exercise/analizeArray.test.js
@@ -8,3 +8,7 @@ test("(2 - Min) [1,8,3,4,2,6] should return 1", () => {
   const result = analizeArray([1, 8, 3, 4, 2, 6]);
   expect(result.min).toBe(1);
 });
+test("(3 - Min) [12,4,6,-1,0,2] should return 1", () => {
+  const result = analizeArray([12, 4, 6, -1, 0, 2]);
+  expect(result.min).toBe(-1);
+});
