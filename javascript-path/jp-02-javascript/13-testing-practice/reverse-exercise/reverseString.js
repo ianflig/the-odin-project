f3864b4ca4efa114cc80a0f194a615a8de5acd27
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-20 21:19:45 -0300

    solve second exercise

---
+++ javascript-path/jp-02-javascript/13-testing-practice/reverse-exercise/reverseString.js
@@ -0,0 +1,5 @@
+// function that takes a string and returns it reversed
+
+export function reverseString(string) {
+  return string.split("").reverse().join("");
+}
---
+++ javascript-path/jp-02-javascript/13-testing-practice/reverse-exercise/reverseString.test.js
@@ -0,0 +1,5 @@
+import { reverseString } from "./reverseString.js";
+
+test("reverse a string", () => {
+  expect(reverseString("apple")).toBe("elppa");
+});
