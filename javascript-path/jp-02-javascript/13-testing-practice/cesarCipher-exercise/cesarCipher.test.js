cfa4574e1b243fd0b9b70b5524d28ce632012759
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-20 23:38:06 -0300

    small changes

--- javascript-path/jp-02-javascript/13-testing-practice/cesarCipher-exercise/cesarCipher.test.js
+++ javascript-path/jp-02-javascript/13-testing-practice/cesarCipher-exercise/cesarCipher.test.js
@@ -1,14 +1,14 @@
 import { cesarCipher } from "./cesarCipher.js";
 
-test("xzy, 3 should return abc", () => {
+test("(1) xzy, 3 should return abc", () => {
   expect(cesarCipher("xyz", 3)).toBe("abc");
 });
-test("HeLLo, 3 should return KhOOr", () => {
+test("(2) HeLLo, 3 should return KhOOr", () => {
   expect(cesarCipher("HeLLo", 3)).toBe("KhOOr");
 });
-test("loPpdD, 4 should return KhOOr", () => {
+test("(3) loPpdD, 4 should return KhOOr", () => {
   expect(cesarCipher("loPpdD", 4)).toBe("psTthH");
 });
-test("Hello, World!, 3 should return Khoor, Zruog!", () => {
+test("(4) Hello, World!, 3 should return Khoor, Zruog!", () => {
   expect(cesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
