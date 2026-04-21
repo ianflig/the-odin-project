92d2d30aeccc9d251a674307706d7a631271b01a
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-20 21:13:22 -0300

    minor change

--- javascript-path/jp-02-javascript/13-testing-practice/capitalize-exercise/capitalize.js
+++ javascript-path/jp-02-javascript/13-testing-practice/capitalize-exercise/capitalize.js
@@ -1,3 +1,5 @@
+// function that takes a string and returns it with the first character capitalized
+
 export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
