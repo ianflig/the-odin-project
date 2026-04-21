6c418b68da4dc19f4cd0d02cf097388b2dfedc2f
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-20 22:23:09 -0300

    refactor

--- javascript-path/jp-02-javascript/13-testing-practice/calculator-exercise/calculator.test.js
+++ javascript-path/jp-02-javascript/13-testing-practice/calculator-exercise/calculator.test.js
@@ -1,23 +1,23 @@
 import { calculator } from "./calculator.js";
 
-test("calculator add", () => {
+test("add 1 + 2 is 3", () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
-test("calculator substract (1)", () => {
+test("add 5 - 3 is 2", () => {
   expect(calculator.substract(5, 3)).toBe(2);
 });
-test("calculator substract (2)", () => {
+test("substract  2 - 5 is -3", () => {
   expect(calculator.substract(2, 5)).toBe(-3);
 });
-test("calculator divide (1)", () => {
+test("divide 10 / 2 is 5", () => {
   expect(calculator.divide(10, 2)).toBe(5);
 });
-test("calculator divide (2)", () => {
+test("divide -50 / 2 is -25", () => {
   expect(calculator.divide(-50, 2)).toBe(-25);
 });
-test("calculator multiply (1)", () => {
+test("multiply 2 * 5 is 10", () => {
   expect(calculator.multiply(2, 5)).toBe(10);
 });
-test("calculator multiply (2)", () => {
-  expect(calculator.multiply(-10, -5)).toBe(50);
+test("multiply -10 * -5 is 50", () => {
+  expect(calculator.multiply(-10, -5)).toBe(25);
 });
