d099d0d1d8376f2da17833181dfbc2dfdc5d6239
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-04 20:29:34 -0300

    adatp to return an array

--- javascript-path/jp-02-javascript/14-recursion/fibonacci/fibonacci-recursive.js
+++ javascript-path/jp-02-javascript/14-recursion/fibonacci/fibonacci-recursive.js
@@ -1,11 +1,15 @@
 function fibsRec(n) {
   if (n === 1) {
-    return 0;
+    return [0];
   }
   if (n === 2) {
-    return 1;
+    return [0, 1];
   }
-  return fibsRec(n - 1) + fibsRec(n - 2);
+  let result = fibsRec(n - 1);
+
+  result.push(result[n - 3] + result[n - 2]);
+
+  return result;
 }
 
 console.log(fibsRec(8)); //[0, 1, 1, 2, 3, 5, 8, 13]
