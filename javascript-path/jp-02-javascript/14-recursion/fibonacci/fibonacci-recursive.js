395ddbbed5d57d3cf6983b03c438b0e70de004e5
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-04 20:10:17 -0300

    adapt base case to get first position === 0

--- javascript-path/jp-02-javascript/14-recursion/fibonacci/fibonacci-recursive.js
+++ javascript-path/jp-02-javascript/14-recursion/fibonacci/fibonacci-recursive.js
@@ -1,9 +1,11 @@
 function fibsRec(n) {
-  if (n < 2) {
-    return n;
-  } else {
-    return fibsRec(n - 1) + fibsRec(n - 2);
+  if (n === 1) {
+    return 0;
   }
+  if (n === 2) {
+    return 1;
+  }
+  return fibsRec(n - 1) + fibsRec(n - 2);
 }
 
-console.log(fibsRec(5)); //[0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsRec(8)); //[0, 1, 1, 2, 3, 5, 8, 13]
