3b79c0ab26a906edce2e2c3f978ebd1f74223c82
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-05 14:37:39 -0300

    solve

--- javascript-path/jp-02-javascript/14-recursion/merge-sort/merge-sort.js
+++ javascript-path/jp-02-javascript/14-recursion/merge-sort/merge-sort.js
@@ -4,12 +4,42 @@ function mergeSort(arr) {
   if (arr.length < 2) {
     return arr;
   }
+  let firstHalf = mergeSort(arr.slice(0, arr.length / 2));
+  let secondHalf = mergeSort(arr.slice(arr.length / 2));
+  let mergedArr = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < firstHalf.length && j < secondHalf.length) {
+    if (firstHalf[i] < secondHalf[j]) {
+      mergedArr.push(firstHalf[i]);
+      i++;
+    } else {
+      mergedArr.push(secondHalf[j]);
+      j++;
+    }
+  }
+
+  while (i < firstHalf.length) {
+    mergedArr.push(firstHalf[i]);
+    i++;
+  }
+
+  while (j < secondHalf.length) {
+    mergedArr.push(secondHalf[j]);
+    j++;
+  }
+
+  return mergedArr;
 }
 
 console.log(mergeSort([])); // []
 console.log(mergeSort([73])); // [73]
-console.log(mergeSort([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5]
 console.log(mergeSort([2, 1])); // [1, 2]
+console.log(mergeSort([1, 2])); // [1, 2]
 console.log(mergeSort([4, 2, 5, 1])); // [1, 2, 5]
-// console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); // [0, 1, 1, 2, 3, 5, 8, 13]
-// console.log(mergeSort([105, 79, 100, 110])) // [79, 100, 105, 110]
+console.log(mergeSort([3, 2, 1, 4])); // [1, 2, 3, 4]
+console.log(mergeSort([1, 2, 3, 4])); // [1, 2, 3, 4]
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(mergeSort([105, 79, 100, 110])); // [79, 100, 105, 110]
