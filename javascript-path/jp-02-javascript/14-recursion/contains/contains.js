9cf94bbf64548d439b19f787b8d9d1123be4e359
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-04 22:45:23 -0300

    test passes

--- javascript-path/jp-02-javascript/14-recursion/contains/contains.js
+++ javascript-path/jp-02-javascript/14-recursion/contains/contains.js
@@ -1,2 +1,23 @@
 // Write a function that searches for a value in a nested object. It returns true if the object contains that value.
 // Objects are compared by reference.
+
+export const contains = function (obj, value) {
+  if (obj === value || (Number.isNaN(obj) && Number.isNaN(value))) {
+    return true;
+  }
+
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+
+  for (let object of Object.values(obj)) {
+    if (contains(object, value)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+console.log(contains({ foo: "foo" }, "bar")); // false
+console.log(contains({ foo: { bar: "bar" } }, "bar")); // true
