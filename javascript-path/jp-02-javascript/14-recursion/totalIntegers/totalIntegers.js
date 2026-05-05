11375b503694786c61c8606ec8047f64758ef521
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-04 22:48:11 -0300

    test passes

--- javascript-path/jp-02-javascript/14-recursion/totalIntegers/totalIntegers.js
+++ javascript-path/jp-02-javascript/14-recursion/totalIntegers/totalIntegers.js
@@ -1,1 +1,19 @@
 // Write a function that takes in an arbitrarily deep array or object and returns the total number of integers stored inside this array or object.
+
+export const totalIntegers = function (obj) {
+  if (typeof obj !== "object" || obj === null) return;
+
+  let total = 0;
+  for (let number of Object.values(obj)) {
+    if (Number.isInteger(number)) {
+      total++;
+    } else {
+      if (typeof totalIntegers(number) === "number")
+        total += totalIntegers(number);
+    }
+  }
+  return total;
+};
+
+console.log(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]])); // returns 7
+console.log(totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } })); // returns 4
