3ad32ed453517f4b5e1fe9b4bb524c25634d58f7
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-04 21:48:12 -0300

    solve

--- javascript-path/jp-02-javascript/14-recursion/factorial/factorial.js
+++ javascript-path/jp-02-javascript/14-recursion/factorial/factorial.js
@@ -1,1 +1,17 @@
 // Write a recursive factorial function that takes a non-negative integer, and returns the product of all positive integers less than or equal to the input integer. An input of 0 should return 1. The function should only accept numbers, so '4' should not be accepted as it is a string. All invalid inputs should return undefined.
+
+const factorial = function (number) {
+  // no need to do "if number is 1 return number", it's covered with 0 validation
+  if (number === 0) {
+    return 1;
+  }
+  if (number < 0 || !Number.isInteger(number)) {
+    return undefined;
+  }
+  return number * factorial(number - 1);
+};
+
+console.log(factorial(5)); // 5 * 4 * 3 * 2 * 1, Output: 120
+console.log(factorial(0)); // Output: 1
+console.log(factorial(7.2)); // Output: undefined
+console.log(factorial("4")); // Output: undefined
