a6af334ed90ab5e40839a40ae42b65e527a4701e
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-12 12:16:32 -0300

    small change

--- javascript-path/jp-02-javascript/16-hash-map/script.js
+++ javascript-path/jp-02-javascript/16-hash-map/script.js
@@ -11,7 +11,8 @@ export class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
     }
 
     return hashCode;
