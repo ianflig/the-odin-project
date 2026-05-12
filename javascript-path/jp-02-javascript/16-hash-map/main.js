845b2a4208ffb6441ddc2cbcd092a9abbd8fd357
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-11 22:08:00 -0300

    add LinkedList & Node class

--- javascript-path/jp-02-javascript/16-hash-map/main.js
+++ javascript-path/jp-02-javascript/16-hash-map/main.js
@@ -2,4 +2,9 @@ import { HashMap } from "./script.js";
 
 const test = new HashMap(16);
 
+// console.log(test.buckets);
+// console.log(test.hash("Sara"));
+test.set("apple", "red");
+// console.log(test.buckets);
+test.set("apple", "red");
 console.log(test.buckets);
--- javascript-path/jp-02-javascript/16-hash-map/script.js
+++ javascript-path/jp-02-javascript/16-hash-map/script.js
@@ -11,9 +11,52 @@ export class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    let hashKey = this.hash(key);
+
+    if (this.buckets[hashKey] === null) {
+      let linkedList = new LinkedList();
+      linkedList.append(key, value);
+      this.buckets[hashKey] = linkedList;
+      return;
+    }
+
+    this.buckets[hashKey].append(key, value);
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this._head = null;
+  }
+
+  append(key, value) {
+    let newNode = new Node(key, value);
+    let currentNode = this._head;
+
+    if (this._head === null) {
+      this._head = newNode;
+      return;
+    }
+
+    while (currentNode.nextNode) {
+      currentNode = currentNode.nextNode;
+    }
+
+    currentNode.nextNode = newNode;
+  }
+}
+
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = null;
   }
 }
