032cab7b2147b0bb631f84bfb4f7002806b3f496
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-11 21:28:06 -0300

    add default hash() & class constructor

---
+++ javascript-path/jp-02-javascript/16-hash-map/main.js
@@ -0,0 +1,5 @@
+import { HashMap } from "./script.js";
+
+const test = new HashMap(16);
+
+console.log(test.buckets);
--- javascript-path/jp-02-javascript/16-hash-map/script.js
+++ javascript-path/jp-02-javascript/16-hash-map/script.js
@@ -2,5 +2,18 @@ export class HashMap {
   constructor(capacity = 16) {
     this.loadFactor = 0.75;
     this.capacity = capacity;
+    this.buckets = new Array(this.capacity).fill(null);
+    this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
   }
 }
