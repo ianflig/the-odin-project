826db1fe6c7beca9f6ee412030ff850e95ecbf2b
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-14 12:46:08 -0300

    #cleanArr method

--- javascript-path/jp-02-javascript/17-binary-search-trees/script.js
+++ javascript-path/jp-02-javascript/17-binary-search-trees/script.js
@@ -8,8 +8,16 @@ class Node {
 
 export class Tree {
   constructor(arr) {
-    this.root = this.#buildTree(arr);
+    this.root = this.#buildTree(this.#clearArr(arr));
   }
 
-  #buildTree(arr) {}
+  #buildTree(arr, start = 0, end = arr.length - 1) {
+    return console.log(arr);
+  }
+
+  #clearArr(arr) {
+    let sortedArr = arr.sort((a, b) => a - b);
+    let cleanArr = [...new Set(sortedArr)];
+    return cleanArr;
+  }
 }
