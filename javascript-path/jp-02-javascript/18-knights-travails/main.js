f01199a5f073031ca5a7afb8e1d8c638606c9332
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-27 16:06:26 -0300

    simple refactor

--- javascript-path/jp-02-javascript/18-knights-travails/main.js
+++ javascript-path/jp-02-javascript/18-knights-travails/main.js
@@ -2,16 +2,20 @@ function knightMoves(from, to) {
   let queue = [];
   let visited = {};
   let found = false;
-  let validLeaps = [];
+  let path = [];
   let temp;
+
   queue.push(from);
 
+  // formatted start (position)
   let startKey = `${from[0]},${from[1]}`;
   visited[startKey] = null;
 
+  // BFS
   while (queue.length > 0 && found === false) {
     let currentPosition = queue.shift();
 
+    // all possible leaps
     let positions = [
       [currentPosition[0] + 2, currentPosition[1] + 1],
       [currentPosition[0] + 1, currentPosition[1] + 2],
@@ -23,8 +27,7 @@ function knightMoves(from, to) {
       [currentPosition[0] - 2, currentPosition[1] - 1],
     ];
 
-    // console.log(currentPosition);
-
+    // filter invalid leaps (out of the 8x8 matrix)
     for (let i = 0; i < positions.length; i++) {
       let currentMove = positions[i];
       if (
@@ -33,14 +36,15 @@ function knightMoves(from, to) {
         currentMove[1] >= 0 &&
         currentMove[1] <= 7
       ) {
+        // format the valid move
         let key = `${currentMove[0]},${currentMove[1]}`;
         let searchedPosition = `${to[0]},${to[1]}`;
-
+        // condition in case it founds the target
         if (key === searchedPosition) {
-          // console.log(currentMove);
           visited[key] = currentPosition;
           found = true;
           break;
+          // store the valid move & filter for next loop
         } else if (!visited[key]) {
           visited[key] = currentPosition;
           queue.push(currentMove);
@@ -49,18 +53,18 @@ function knightMoves(from, to) {
     }
   }
 
+  // print movements section
   temp = `${to[0]},${to[1]}`;
 
   while (temp !== startKey) {
-    validLeaps.push(temp);
+    path.push(temp);
     temp = `${visited[temp][0]},${visited[temp][1]}`;
   }
 
-  validLeaps.push(startKey);
+  path.push(startKey);
 
-  // console.log(visited);
-  return validLeaps.reverse();
+  console.log(`You made it in ${path.length - 1} moves. Path:`);
+  console.log(path.reverse());
 }
 
-knightMoves([0, 0], [7, 7]);
-console.log(knightMoves([0, 0], [7, 7]));
+knightMoves([3, 3], [4, 3]);
