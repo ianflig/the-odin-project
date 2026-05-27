3f66915c1fc23d58c532d513ec9ee1c13b8b1d87
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-27 15:50:09 -0300

    add simple output

--- javascript-path/jp-02-javascript/18-knights-travails/main.js
+++ javascript-path/jp-02-javascript/18-knights-travails/main.js
@@ -1,18 +1,16 @@
-function checkVisited(visitedList, key) {
-  return visitedList[key] === true;
-}
-
 function knightMoves(from, to) {
   let queue = [];
   let visited = {};
   let found = false;
+  let validLeaps = [];
+  let temp;
   queue.push(from);
+
+  let startKey = `${from[0]},${from[1]}`;
+  visited[startKey] = null;
 
   while (queue.length > 0 && found === false) {
     let currentPosition = queue.shift();
-
-    let currentKey = `${currentPosition[0]},${currentPosition[1]}`;
-    visited[currentKey] = true;
 
     let positions = [
       [currentPosition[0] + 2, currentPosition[1] + 1],
@@ -25,7 +23,7 @@ function knightMoves(from, to) {
       [currentPosition[0] - 2, currentPosition[1] - 1],
     ];
 
-    console.log(currentPosition);
+    // console.log(currentPosition);
 
     for (let i = 0; i < positions.length; i++) {
       let currentMove = positions[i];
@@ -39,69 +37,30 @@ function knightMoves(from, to) {
         let searchedPosition = `${to[0]},${to[1]}`;
 
         if (key === searchedPosition) {
-          console.log(currentMove);
+          // console.log(currentMove);
+          visited[key] = currentPosition;
           found = true;
           break;
         } else if (!visited[key]) {
-          visited[key] = true;
+          visited[key] = currentPosition;
           queue.push(currentMove);
         }
       }
     }
   }
 
-  console.log(visited);
+  temp = `${to[0]},${to[1]}`;
+
+  while (temp !== startKey) {
+    validLeaps.push(temp);
+    temp = `${visited[temp][0]},${visited[temp][1]}`;
+  }
+
+  validLeaps.push(startKey);
+
+  // console.log(visited);
+  return validLeaps.reverse();
 }
 
-// console.log(knightMoves([0, 0]));
-knightMoves([0, 0], [4, 4]);
-
-// let visited = {
-//   "2,1": true,
-//   "3,4": true,
-//   "6,5": true,
-// };
-
-// let arr = [8, 4];
-
-// let key = `${arr[0]},${arr[1]}`;
-// visited[key] = true;
-
-// let newKey = "8,5";
-// visited[newKey] = false;
-
-// if (!visited[newKey] || visited[newKey] !== true) console.log(true);
-
-// console.log(visited);
-// console.log(checkVisited(visited, key));
-
-// let a = [1, 2];
-// let b = [1, 2];
-
-// console.log(a[0] === b[0] && a[1] === b[1]);
-
-// < 0 || > 7  = invalid movement
-
-// 1) y = number + 2
-//    x = number + 1
-
-// 2) y = number + 1
-//    x = number + 2
-
-// 3) y = number - 1
-//    x = number + 2
-
-// 4) y = number - 2
-//    x = number + 1
-
-// 5) y = number + 2
-//    x = number - 1
-
-// 6) y = number + 1
-//    x = number - 2
-
-// 7) y = number - 1
-//    x = number - 2
-
-// 8) y = number - 2
-//    x = number - 1
+knightMoves([0, 0], [7, 7]);
+console.log(knightMoves([0, 0], [7, 7]));
