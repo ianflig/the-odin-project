be413a32dff67c84b9b0e248c88ed08bd203faaa
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-27 14:54:10 -0300

    get BFS and visited vertices filter working

--- javascript-path/jp-02-javascript/18-knights-travails/main.js
+++ javascript-path/jp-02-javascript/18-knights-travails/main.js
@@ -1,11 +1,19 @@
+function checkVisited(visitedList, key) {
+  return visitedList[key] === true;
+}
+
 function knightMoves(from, to) {
   let queue = [];
+  let visited = {};
   let found = false;
   queue.push(from);
 
   while (queue.length > 0 && found === false) {
-    // let availableLeaps = [];
-    let currentPosition = queue[0];
+    let currentPosition = queue.shift();
+
+    let currentKey = `${currentPosition[0]},${currentPosition[1]}`;
+    visited[currentKey] = true;
+
     let positions = [
       [currentPosition[0] + 2, currentPosition[1] + 1],
       [currentPosition[0] + 1, currentPosition[1] + 2],
@@ -27,23 +35,45 @@ function knightMoves(from, to) {
         currentMove[1] >= 0 &&
         currentMove[1] <= 7
       ) {
-        if (currentMove[0] === to[0] && currentMove[1] === to[1]) {
+        let key = `${currentMove[0]},${currentMove[1]}`;
+        let searchedPosition = `${to[0]},${to[1]}`;
+
+        if (key === searchedPosition) {
           console.log(currentMove);
-          return (found = true);
-        } else {
+          found = true;
+          break;
+        } else if (!visited[key]) {
+          visited[key] = true;
           queue.push(currentMove);
         }
       }
     }
-
-    queue.shift();
   }
 
-  // return availableLeaps;
+  console.log(visited);
 }
 
 // console.log(knightMoves([0, 0]));
-knightMoves([0, 0], [4, 3]);
+knightMoves([0, 0], [4, 4]);
+
+// let visited = {
+//   "2,1": true,
+//   "3,4": true,
+//   "6,5": true,
+// };
+
+// let arr = [8, 4];
+
+// let key = `${arr[0]},${arr[1]}`;
+// visited[key] = true;
+
+// let newKey = "8,5";
+// visited[newKey] = false;
+
+// if (!visited[newKey] || visited[newKey] !== true) console.log(true);
+
+// console.log(visited);
+// console.log(checkVisited(visited, key));
 
 // let a = [1, 2];
 // let b = [1, 2];
