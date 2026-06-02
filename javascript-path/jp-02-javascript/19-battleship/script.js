d72ae4f71e2908694043f9f89bc6d600d0c4c8a0
ianflig <129326345+ianflig@users.noreply.github.com>
2026-06-02 13:30:38 -0300

    add Ship class

--- javascript-path/jp-02-javascript/19-battleship/script.js
+++ javascript-path/jp-02-javascript/19-battleship/script.js
@@ -1,3 +1,19 @@
 class Ship {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.hits = 0;
+    this._isSunk = false;
+  }
+
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    if (this.hits === this.length) {
+      this._isSunk = true;
+    }
+
+    return this._isSunk;
+  }
 }
