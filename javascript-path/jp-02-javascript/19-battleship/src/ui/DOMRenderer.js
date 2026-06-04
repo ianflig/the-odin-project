5898da62eb4556b1f4a15fc6a1c539f890fdb62d
ianflig <129326345+ianflig@users.noreply.github.com>
2026-06-04 17:06:57 -0300

    WIP

--- javascript-path/jp-02-javascript/19-battleship/src/ui/DOMRenderer.js
+++ javascript-path/jp-02-javascript/19-battleship/src/ui/DOMRenderer.js
@@ -2,14 +2,12 @@ export class Renderer {
   constructor() {}
 
   renderGameboards(size) {
-    const playerOneGameboard = document.querySelector(".player-one-gameboard");
-    const playerTwoGameboard = document.querySelector(".player-two-gameboard");
-    for (let i = 0; i < size * size; i++) {
-      let div = document.createElement("div");
-      div.textContent = "test";
-      div.classList.add("gameboard-cell");
-      playerOneGameboard.append(div);
-      playerTwoGameboard.append(div);
-    }
+    const playerOneGameboard = `<div class="gameboard-cell">test</div>`;
+    const playerTwoGameboard = `<div class="gameboard-cell">test</div>`;
+
+    document.querySelector(".player-one-gameboard").innerHTML =
+      playerOneGameboard.repeat(size * size);
+    document.querySelector(".player-two-gameboard").innerHTML =
+      playerTwoGameboard.repeat(size * size);
   }
 }
