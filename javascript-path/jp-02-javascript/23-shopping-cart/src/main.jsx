e37397754b3be8a3ebcaee55c304ec7c65f160a0
ianflig <129326345+ianflig@users.noreply.github.com>
2026-09-17 21:09:36 -0300

    react-router + basic routing

--- javascript-path/jp-02-javascript/23-shopping-cart/src/main.jsx
+++ javascript-path/jp-02-javascript/23-shopping-cart/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import "./index.css";
+import { AppLayout } from "./AppLayout.jsx";
+import { Home } from "./Home";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Navigate to="/home" replace />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="*" element={<Navigate to="/home"> </Navigate>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
