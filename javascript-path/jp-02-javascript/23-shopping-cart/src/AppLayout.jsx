293107921165eb2bbb82fcfca03e80bac2b9689e
ianflig <129326345+ianflig@users.noreply.github.com>
2026-09-19 00:27:14 -0300

    childs

--- javascript-path/jp-02-javascript/23-shopping-cart/src/AppLayout.jsx
+++ javascript-path/jp-02-javascript/23-shopping-cart/src/AppLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
+import { useState } from "react";
 import { Header } from "./Header";
 import { SECTIONS } from "./constants.js";
 
 export function AppLayout() {
+  const [products, setProducts] = useState();
+  const [cart, setCart] = useState();
+
   return (
     <section className="flex flex-col">
       <Header sections={SECTIONS} />
-      <Outlet />
+      <Outlet context={[products, setProducts, cart, setCart]} />
     </section>
   );
 }
--- javascript-path/jp-02-javascript/23-shopping-cart/src/Shop.jsx
+++ javascript-path/jp-02-javascript/23-shopping-cart/src/Shop.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
+import { useOutletContext } from "react-router";
+
 export function Shop() {
+  const [products, setProducts] = useOutletContext();
+
+  useEffect(() => {
+    if (products) return;
+    const fetchURL = "https://dummyjson.com/products";
+    async function fetchProducts() {
+      const res = await fetch(fetchURL).then((res) => res.json());
+      res ? setProducts(res.products) : undefined;
+      console.log(res.products);
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <span>Shop</span>
+      <div className="grid-cols[repeat(auto-fit, minmax(120px, 1fr))] grid">
+        {products ? <span>{products[0].title}</span> : undefined}
+      </div>
     </>
   );
 }
